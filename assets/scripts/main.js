68e0381de05108a7a43734af487443df5e1fcefe
Anshul Singh <anshul@neevtech.com>
2016-01-25 18:23:42 +0530

    Code refactor for basic and multiple parallax & updated README for same

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,23 +1,25 @@
-var ignoreHeight = document.getElementById('page-heading').clientHeight + document.getElementById('first-para').clientHeight;
-document.getElementsByClassName('dark-circle')[0].style.bottom = -(document.getElementsByClassName('dark-circle')[0].clientHeight) + "px";
-var circleBottom = document.getElementsByClassName('dark-circle')[0].style.bottom.match(/\d/g).join("");
-var darkCircleOffset = 40;
+var basicParallaxOffset = document.getElementById('page-heading').clientHeight + document.getElementById('first-para').clientHeight;
+document.getElementsByClassName('basic-parallax-panel')[0].style.height = window.innerHeight - basicParallaxOffset + 'px';
+document.getElementsByClassName('moving-background')[0].style.height = window.innerHeight - basicParallaxOffset + 'px';
 
+//Bind Scroll event
+window.addEventListener("scroll", parallaxScroll, false);
+
+//handling parallax effect
 function parallaxScroll() {
-	document.getElementsByClassName('moving-background')[0].style.top = -(window.pageYOffset / 5 - ignoreHeight ) + 'px';
-	var opacityDiv = 1 - window.pageYOffset / window.innerHeight;
-	document.getElementsByClassName('parallax-content')[0].style.opacity = opacityDiv;
-	console.log(window.pageYOffset);
-	console.log(document.getElementsByTagName('body')[0].clientHeight - window.innerHeight - document.getElementsByClassName('form')[0].clientHeight - document.getElementsByClassName('content-end')[0].clientHeight + darkCircleOffset);
+	var opacityOfParallaxObject = 1 - window.pageYOffset / window.innerHeight;
+	var darkCircleOffset = 40;
+
+	//Primary Parallax code
+	document.getElementsByClassName('moving-background')[0].style.top = -(window.pageYOffset / 5 - basicParallaxOffset ) + 'px';
+	document.getElementsByClassName('parallax-content')[0].style.opacity = opacityOfParallaxObject;
+
+	//Secondary Parallax code
 	if(window.pageYOffset > document.getElementsByTagName('body')[0].clientHeight - window.innerHeight - document.getElementsByClassName('form')[0].clientHeight - document.getElementsByClassName('content-end')[0].clientHeight + darkCircleOffset) {
 		document.getElementsByClassName('dark-circle')[0].style.bottom = ((window.pageYOffset - (document.getElementsByTagName('body')[0].clientHeight - window.innerHeight - document.getElementsByClassName('form')[0].clientHeight - document.getElementsByClassName('content-end')[0].clientHeight + darkCircleOffset)) / 2) + "px";
-		document.getElementsByClassName('dark-circle')[0].style.opacity = -opacityDiv / 1.5;
+		document.getElementsByClassName('dark-circle')[0].style.opacity = -opacityOfParallaxObject / 1.5;
 	}
 	if(window.pageYOffset < document.getElementsByTagName('body')[0].clientHeight - window.innerHeight - document.getElementsByClassName('form')[0].clientHeight - document.getElementsByClassName('content-end')[0].clientHeight + darkCircleOffset) {
-		document.getElementsByClassName('dark-circle')[0].style.bottom = -circleBottom + "px";
+		document.getElementsByClassName('dark-circle')[0].style.bottom = -(document.getElementsByClassName('dark-circle')[0].clientHeight) + "px";
 	}
 }
-
-window.addEventListener("scroll", parallaxScroll, false);
-document.getElementsByClassName('main-panel')[0].style.height = window.innerHeight - ignoreHeight + 'px';
-document.getElementsByClassName('moving-background')[0].style.height = document.getElementsByClassName('main-panel')[0].clientHeight + "px";
