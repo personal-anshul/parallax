2305c4a66595c8b807c3c6ddaecaffae05818332
personal_anshul <sfwr.er.ansh@gmail.com>
2016-01-28 11:41:13 +0530

    Code modification for navigation bar

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,4 +1,15 @@
 jQuery(document).ready(function ($) {
-    //initialise Stellar.js
-    $(window).stellar();
+  //initialise Stellar.js
+  $(window).stellar();
+
+  //scroll to give section
+  var element = $('li');
+  element.click(function() {
+    element.removeClass('link-active');
+    var targetElement = $(this).attr('scroll-to');
+    $('body').animate({
+      scrollTop: $('.slide[id="' + targetElement + '"]').offset().top
+    }, 2000, 'easeInOutQuint');
+    $(this).addClass('link-active');
+  });
 });
