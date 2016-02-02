e40c92509e7bb89259e89e27ea0816a5e61bfee2
personal_anshul <sfwr.er.ansh@gmail.com>
2016-02-03 01:57:21 +0530

    Code refactor and code modification for down - up arrow

--- assets/scripts/parallax-stellar.js
+++ assets/scripts/parallax-stellar.js
@@ -8,7 +8,7 @@ jQuery(document).ready(function ($) {
     var targetElement = $(this).attr('scroll-to');
     $('html, body').animate({
       scrollTop: $('.slide[id="' + targetElement + '"]').offset().top
-    }, 2000, 'easeInOutQuint');
+    }, 3000, 'easeOutQuart');
   });
 
   //Change Selected link on scroll
@@ -17,7 +17,6 @@ jQuery(document).ready(function ($) {
     var navBarHeight = 45;
     for(var targetElement = 1; targetElement < 5; targetElement++ ) {
       var offsetOfSlide = $("#slide" + targetElement).offset().top;
-      console.log(window.pageYOffset + " and " + offsetOfSlide);
       if(window.pageYOffset + navBarHeight > offsetOfSlide) {
         linkElements.removeClass('link-active');
         $('li[scroll-to="slide' + targetElement + '"').addClass('link-active');
